13a65a8d4973ecd84cc5b7ef6e437bbb772b50dd
arshia.mkh-ubuntu <arshia.mokhlesi@gmail.com>
2023-06-09 13:46:01 +0330

    complete operation asked for ch12 and ch1

--- OS.c
+++ OS.c
@@ -24,6 +24,9 @@ int main() {
   
   }else if(ch1 == 0){// ch1 process
 
+  int factorial ;
+  int square_sum ;
+
     //Note: creation pipe ch1, ch11
     int pipels1_11[2];
     // pipels1-11[0] : read
@@ -57,17 +60,77 @@ int main() {
       // ch1 process
 
       close(pipels1_11[1]); // close the unused read side of pipe 
-      int square_sum ;
+      
 
       read(pipels1_11[0], &square_sum, sizeof(int));
       printf("this is the square sums : %d\n", square_sum);
       close(pipels1_11[0]);
+      int status;
 
-      exit(1);
+      waitpid(ch11, &status, 0);
     }
+
+  int pipels1_12[2];
+
+  if(pipe(pipels1_12) == -1){
+    printf("pipe ch1 ,ch12 cration faild");
+    exit(12);
+  }
+
+    //Note: go into ch12 process
+
+  pid_t ch12 = fork();
+    
+  if (ch12 < 0){
+    printf("ch12 creation process faild");
+    }else if (ch12 == 0){
+      
+      //Note: ch12 process
+
+      close(pipels1_12[0]); // close the unused read side of pipe
+
+      int i, factorial = 1;
+      int temp = 1 ;
+      int temp2 ;
+      for(i = 2; i < a; i++){
+        temp2 = factorial;
+        factorial += temp ;
+        temp = temp2 ;
+      }
+
+      write(pipels1_12[1], &factorial, sizeof(int)); 
+      close(pipels1_12[1]);
+
+      exit(12);
+    }else {
+      // ch1 process
+
+      close(pipels1_12[1]); // close the unused read side of pipe 
+      
+
+      read(pipels1_12[0], &factorial, sizeof(int));
+      printf("this is the factorial : %d\n", factorial);
+      close(pipels1_12[0]);
+
+      int status;
+      waitpid(ch12, &status, 0);
+    }
+
+    int inputch1;
+
+    scanf("%d", &inputch1);
+
+    printf("ch1 output: %d\n", (inputch1 + factorial + square_sum));
+
 
     exit(1);
   }
+
+  int status;
+
+  waitpid(ch1, &status, 0);
+
+  
 
   // }else{ // parent process
   //   // wait(NULL);
